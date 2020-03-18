ebc76f8aff75d8fe671bc4bd56c43989070826bb
nickg <nickg@client9.com>
2020-03-17 22:09:12 -0700

    fix json parse

--- index.js
+++ index.js
@@ -3,12 +3,17 @@ const humanizeDuration = require('humanize-duration');
 // https://www.npmjs.com/package/humanize-duration
 try {
   var millis = parseInt(core.getInput('millis'));
+  var config = {};
+  var config_str = core.getInput('config');
+  if (!!config_str) {
+    config = eval('config = ' + config_str);
+  }
   if (isNaN(millis)) {
 	  var now = Date.now();
 	  var from = Date.parse(core.getInput('from'));
 	  millis = now - from;
   }
-  core.setOutput("duration", humanizeDuration(millis, JSON.parse(core.getInput('config'))));
+  core.setOutput("duration", humanizeDuration(millis, config));
 } catch (error) {
   core.setFailed(error.message);
 }
