35f72141cb9b929d1145f25dfc4bc8d5558a2d6f
nickg <nickg@client9.com>
2020-03-17 21:42:30 -0700

    add "from" duration

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const humanizeDuration = require('humanize-duration');
 // https://www.npmjs.com/package/humanize-duration
 try {
   var millis = parseInt(core.getInput('millis'));
+  if (isNaN(millis)) {
+	  var now = Date.now();
+	  var from = Data.parse(core.getInput('from'));
+	  millis = now - from;
+  }
   core.setOutput("duration", humanizeDuration(millis));
 } catch (error) {
   core.setFailed(error.message);
